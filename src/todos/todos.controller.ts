da0146416e762bff1b32f9aa8ae75c73433a84a8
King Elisha <ek.chibueze@gmail.com>
2020-04-22 16:49:23 +0100

    createTodo

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body } from '@nestjs/common';
+import { CreateTodoDto } from './dto/create-todo.dto';
 
 @Controller('todos')
 export class TodosController {
   @Get()
   getAll() {
     return 'All Todos';
+  }
+
+  @Post()
+  createTodo(@Body() todoDto: CreateTodoDto) {
+    return `Created Todo with title: ${todoDto.title}`;
   }
 }
