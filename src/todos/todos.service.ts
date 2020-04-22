bf7595c3a9692d13535c28c1ac54c7df850dbe30
King Elisha <ek.chibueze@gmail.com>
2020-04-22 17:36:50 +0100

    data

--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { Todo } from './interfaces/todo.interface';
 
 @Injectable()
-export class TodosService {}
+export class TodosService {
+  private readonly todos: Todo[] = [
+    {
+      id: 'sjncjndcjs',
+      title: 'Todo 1',
+      description: 'More about Todo 1',
+      date: Date.now(),
+      done: false,
+    },
+    {
+      id: 'whye671hqn',
+      title: 'Todo 2',
+      description: 'More about Todo 2',
+      date: Date.now(),
+      done: false,
+    },
+    {
+      id: 'wwuywhsts8',
+      title: 'Todo 3',
+      description: 'More about Todo 3',
+      date: Date.now(),
+      done: false,
+    },
+  ];
+}
