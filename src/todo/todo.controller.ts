fc845ba1b2c2d1e4a63cdbbb28d94ef15313a471
King Elisha <ek.chibueze@gmail.com>
2020-04-22 16:35:29 +0100

    get all todos

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
 
 @Controller('todo')
-export class TodoController {}
+export class TodoController {
+  @Get()
+  getAll() {
+    return 'All Todos';
+  }
+}
